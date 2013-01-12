0adbc110356c5e6818cadd7ded0dfa1bb4b1f23a
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-01-12 17:01:20 +0100

    removed validation and check for keys, should be done by Specifications!

--- ValueObject.php
+++ ValueObject.php
@@ -6,36 +6,11 @@ class ValueObject
 {
     private $values;
     
-    function __construct($values, $allowedKeys=false, $validationService=false)
+    function __construct($values)
     {
-        if ($allowedKeys) {
-            $this->checkAllowedKeys($values, $allowedKeys);
-        }
-        if ($validationService) {
-            $this->validate($values, $validationService);
-        }
         $this->values = $values;
     }
 
-    public function validate($values, $validationService)
-    {
-        $validationService->setValues($values);
-        $valid = $validationService->validate();
-        if (!$valid) {
-            throw new \Exception('event data is not valid: validation errors');
-        }
-    }
-    
-    public function checkAllowedKeys($values, $allowedKeys)
-    {
-        foreach($values as $key => $value)
-        {
-            if (!in_array($key, $allowedKeys)) {
-                throw new \Exception('event data is not valid: invalid key(s)');
-            }
-        }
-    }    
-    
     function getValueByKey($key)
     {
         return $this->values[$key];
